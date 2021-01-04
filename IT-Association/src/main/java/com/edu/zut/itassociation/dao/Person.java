fd838c9481ca6444c1fb67c75634981403e7fb4a
刘玉林 <1017611768@qq.com>
2021-01-04 14:25:30 +0800

    主pom和yml配置文件

--- IT-Association/src/main/java/com/edu/zut/itassociation/dao/Person.java
+++ IT-Association/src/main/java/com/edu/zut/itassociation/dao/Person.java
@@ -4,8 +4,9 @@ package com.edu.zut.itassociation.dao;
  * @author lyl
  * @program: IT-Association
  * @package com.edu.zut.itassociation.dao
- * @description:
+ * @description:测试
  * @date 2021-01-04 11:18
  **/
 public class Person {
+
 }
