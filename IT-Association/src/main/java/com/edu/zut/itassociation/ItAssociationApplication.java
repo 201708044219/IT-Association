f95112d4d73e402e7e27fd94be0283b2a93877bd
刘玉林 <1017611768@qq.com>
2021-01-04 11:05:20 +0800

    毕业设计项目初构

---
+++ IT-Association/src/main/java/com/edu/zut/itassociation/ItAssociationApplication.java
@@ -0,0 +1,13 @@
+package com.edu.zut.itassociation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItAssociationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItAssociationApplication.class, args);
+    }
+
+}
---
+++ IT-Association/src/test/java/com/edu/zut/itassociation/ItAssociationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.zut.itassociation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItAssociationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
