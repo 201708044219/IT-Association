1af0d30900d258c22374f6108436b62f2f67df03
刘玉林 <1017611768@qq.com>
2021-01-04 11:19:47 +0800

    简单测试git连接

---
+++ IT-Association/src/main/java/com/edu/zut/itassociation/dao/Person.java
@@ -0,0 +1,11 @@
+package com.edu.zut.itassociation.dao;
+
+/**
+ * @author lyl
+ * @program: IT-Association
+ * @package com.edu.zut.itassociation.dao
+ * @description:
+ * @date 2021-01-04 11:18
+ **/
+public class Person {
+}
